ce9e6dc08f0fac6c12b78c7f7ddd1099ec1453db
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-02-07 16:45:35 +0700

    feat: add name comp and system

--- packages/contracts/src/components/FactionComponent.sol
+++ packages/contracts/src/components/FactionComponent.sol
@@ -2,8 +2,8 @@
 pragma solidity >=0.8.0;
 import "std-contracts/components/Uint32Component.sol";
 
-uint256 constant ID = uint256(keccak256("component.Counter"));
+uint256 constant ID = uint256(keccak256("component.Faction"));
 
-contract CounterComponent is Uint32Component {
+contract FactionComponent is Uint32Component {
   constructor(address world) Uint32Component(world, ID) {}
 }
