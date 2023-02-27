e69d411e8489d3e3e48faab526e7dfec35ba4269
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-02-27 12:57:55 +0700

    feat: update hasher in wasm

--- packages/circuits/src/wasm/hasher.rs
+++ packages/circuits/src/wasm/hasher.rs
@@ -1,20 +1,25 @@
 use std::panic;
 
 use ark_bn254::Fr;
-use arkworks_native_gadgets::poseidon::{FieldHasher, Poseidon};
+use ark_ff::Zero;
+use arkworks_mimc::{
+    params::{
+        mimc_7_91_bn254::{MIMC_7_91_BN254_PARAMS, MIMC_7_91_BN254_ROUND_KEYS},
+        round_keys_contants_to_vec,
+    },
+    MiMC,
+};
+use arkworks_native_gadgets::poseidon::FieldHasher;
 use wasm_bindgen::prelude::wasm_bindgen;
 
-use crate::{
-    utils::setup_poseidon,
-    wasm::{bf, fr},
-};
+use crate::wasm::{bf, fr};
 
 use super::fri;
 
 #[wasm_bindgen]
 #[allow(dead_code)]
 pub struct Hasher {
-    hasher: Poseidon<Fr>,
+    hasher: MiMC<Fr, MIMC_7_91_BN254_PARAMS>,
 }
 
 #[wasm_bindgen]
@@ -23,31 +28,39 @@ impl Hasher {
     pub fn new() -> Self {
         panic::set_hook(Box::new(console_error_panic_hook::hook));
         Self {
-            hasher: setup_poseidon(),
+            hasher: MiMC::new(
+                1,
+                Fr::zero(),
+                round_keys_contants_to_vec(&MIMC_7_91_BN254_ROUND_KEYS),
+            ),
         }
     }
 
     pub fn hash_one(&self, v: &str) -> String {
-        bf(self.hasher.hash(&[fr(v)]).unwrap())
+        bf(self.hasher.permute_non_feistel(vec![fr(v)])[0])
     }
 
     pub fn hash_two(&self, v1: &str, v2: &str) -> String {
-        bf(self.hasher.hash(&[fr(v1), fr(v2)]).unwrap())
+        bf(self.hasher.permute_non_feistel(vec![fr(v1), fr(v2)])[0])
     }
 
     pub fn hash_three(&self, v1: &str, v2: &str, v3: &str) -> String {
-        bf(self.hasher.hash(&[fr(v1), fr(v2), fr(v3)]).unwrap())
+        bf(self
+            .hasher
+            .permute_non_feistel(vec![fr(v1), fr(v2), fr(v3)])[0])
     }
 
     pub fn hash_one_i(&self, v: i64) -> String {
-        bf(self.hasher.hash(&[fri(v)]).unwrap())
+        bf(self.hasher.permute_non_feistel(vec![fri(v)])[0])
     }
 
     pub fn hash_two_i(&self, v1: i64, v2: i64) -> String {
-        bf(self.hasher.hash(&[fri(v1), fri(v2)]).unwrap())
+        bf(self.hasher.permute_non_feistel(vec![fri(v1), fri(v2)])[0])
     }
 
     pub fn hash_three_i(&self, v1: i64, v2: i64, v3: i64) -> String {
-        bf(self.hasher.hash(&[fri(v1), fri(v2), fri(v3)]).unwrap())
+        bf(self
+            .hasher
+            .permute_non_feistel(vec![fri(v1), fri(v2), fri(v3)])[0])
     }
 }
