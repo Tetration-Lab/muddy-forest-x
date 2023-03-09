adee119732d57615abd4d2fd666ea68da8b58e3f
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-03-09 15:37:43 +0700

    feat: adjust rarity and planet level

--- packages/contracts/src/constants/level.sol
+++ packages/contracts/src/constants/level.sol
@@ -2,5 +2,5 @@
 pragma solidity >=0.8.0;
 
 function PLANET_LEVEL(uint256 location) pure returns (uint32 level) {
-  return uint32(location) ^ 0xFFFFFF % 10;
+  return (uint32(location) ^ 0xF0F0F0) % 10;
 }
