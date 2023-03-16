38816007b037a0e5935823fadad70f0f6fe9c723
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-03-16 13:24:27 +0700

    feat: change material name and pic

--- packages/client/src/const/materials.ts
+++ packages/client/src/const/materials.ts
@@ -2,12 +2,12 @@ import { Material } from '../types/item'
 
 export const MATERIALS: { [key in string]: Material } = {
   '0x9f56a1af35c8caed4f9734f6062d93735763f6799f9a273891339650060de839': {
-    name: 'Iridium',
+    name: 'Stellar Prism',
     imageUrl: './assets/svg/materials/item1.svg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   '0x9f56a1af35c8caed4f9734f6062d93735763f6799f9a273891339650060de83a': {
-    name: 'Plutonium',
+    name: 'Astral Nebula',
     imageUrl: './assets/svg/materials/item2.svg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
