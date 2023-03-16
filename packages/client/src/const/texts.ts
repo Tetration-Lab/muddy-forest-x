540cdf3ba2e6e02d7f76033f5c7cba20676fbdcf
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-03-16 14:34:34 +0700

    fix: correct intro text

--- packages/client/src/const/texts.ts
+++ packages/client/src/const/texts.ts
@@ -1,8 +1,7 @@
 export const texts = {
-  intro1:
-    'First time right ?\n\nI will introduce you some background of “Muddy forest”....\nSince human can reach space, they’re gone far away to finding new and more advance resources...',
+  intro1: 'First time right ?\n\nI will introduce you some background of “Muddy forest”....',
   intro2:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.',
+    'In the year 2150, humanity had finally mastered space travel and begun to colonize nearby planets. However, they were not alone in the universe. As they expanded their reach, they encountered two other advanced civilizations: a race of intelligent and advanced aliens, and a powerful and self-evolving AI system.\n\nEach faction has their own headquarters and must build, upgrade and deploy their fleets of spacecraft to invade and claim ownership of planets. Players choose a faction and engage in epic battles across the universe, fighting for dominance and strategic resource control.',
   displayNameDescription:
     'Before you start playing our game, we need you to bind a unique name to your account. This will help you to easily identify your account and keep track of your progress.',
   infoBeforeStart:
